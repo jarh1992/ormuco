cadbdec42d17dad54eea3d4aab98ba52e7f550d4
jesus <jarh1992@gmail.com>
2017-06-16 20:26:47 -0500

    disable debug mode

--- ormuco/settings.py
+++ ormuco/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7xl2xqzaa05x=+=_t==jmb7fw9+xnc8dr@$vvtv52-zb*4oup*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
